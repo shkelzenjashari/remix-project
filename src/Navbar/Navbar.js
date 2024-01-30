0f2c8e431d517c67ba4b3563f617e1d945fda4de
shkelzenjashari <jasharishkelzen@gmail.com>
2024-01-30 02:33:56 +0100

    navbar-test

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../images/remixLOGO.png";
 
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="Remix-Gaming-Center-Logo" />
+      <div className="list">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
