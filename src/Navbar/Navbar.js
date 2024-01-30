8ddd13c86d7a22dc1bd045c8700ffbfcc2d74ca2
shkelzenjashari <jasharishkelzen@gmail.com>
2024-01-30 03:26:09 +0100

    hamburger-menu

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../images/remixLOGO.png";
+import hamburgerMenu from "../images/hamburgerMenu.png";
 
 const Navbar = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpened(!menuOpened);
+  };
+
   return (
     <div className="navbar">
-      <img src={logo} alt="Remix-Gaming-Center-Logo" />
-      <div className="list">
-        <ul>
+      <img className="logo" src={logo} alt="Remix-Gaming-Center-Logo" />
+      <div className={`list ${menuOpened ? "active" : ""}`}>
+        <ul className={`ul ${menuOpened ? "active" : ""}`}>
           <li>
             <a href="#home">Home</a>
           </li>
@@ -19,6 +26,12 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+      <img
+        className="hamburgerMenu"
+        onClick={toggleMenu}
+        src={hamburgerMenu}
+        alt="hamburger-menu"
+      />
     </div>
   );
 };
