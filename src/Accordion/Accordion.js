b96685ae9b80d34b1b650363da7cdac4da481f0d
shkelzenjashari <jasharishkelzen@gmail.com>
2024-01-31 02:35:49 +0100

    slider, aboutUs

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
 import "./accordion.css";
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
 
 const Accordion = () => {
-  return <div>Accordion</div>;
+  const photos = [image1, image2, image3];
+  return (
+    <div className="accordion">
+      <Slider autoplay={1500}>
+        {photos.map((photo, index) => (
+          <div key={index}>
+            <img src={photo} className="img-responsive" alt="remix" />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default Accordion;
