5d6985b7b9a1a680bffc61e5cc183d2bc28c620e
shkelzenjashari <jasharishkelzen@gmail.com>
2024-02-02 01:10:10 +0100

    .

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -27,8 +27,8 @@ const ContactUs = () => {
           </div>
           <div className="form-third">
             <p>
-              This site is protected by reCAPTCHA and the Google <br /> Privacy
-              Policy and Terms of Service apply.
+              This site is protected by reCAPTCHA <br /> and the Google Privacy
+              Policy <br /> and Terms of Service apply.
             </p>
             <button type="submit">Send</button>
           </div>
