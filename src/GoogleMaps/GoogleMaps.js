ad08d7fb1418575586ae75b54444ae0ea8656d79
rilindselmani <rilindselmani01@gmail.com>
2024-02-02 02:38:58 +0100

    #

--- src/GoogleMaps/GoogleMaps.js
+++ src/GoogleMaps/GoogleMaps.js
@@ -1,41 +1,11 @@
 import React from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-const libraries = ["places"];
-const mapContainerStyle = {
-  width: "100vw",
-  height: "100vh",
-};
-const center = {
-  lat: 7.2905715, // default latitude
-  lng: 80.6337262, // default longitude
-};
-
-const GoogleMapp = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyBzuSUXAaooIVGPufyJhciYhkwcmyGmETw",
-    libraries,
-  });
-
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
-
-  if (!isLoaded) {
-    return <div>Loading maps</div>;
-  }
-
+const GoogleMaps = () => {
   return (
-    <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        center={center}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </div>
+    <>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2933.64131933652!2d21.157068376539296!3d42.668955715529854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549f214c9cabb1%3A0xb223c9c6e50d4cea!2sRemix%20Gaming%20Center!5e0!3m2!1sen!2s!4v1706835208892!5m2!1sen!2s"></iframe>
+    </>
   );
 };
 
-export default GoogleMapp;
+export default GoogleMaps;
