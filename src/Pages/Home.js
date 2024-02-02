71f588d37198eb67376de17988360a954d5a0439
shkelzenjashari <jasharishkelzen@gmail.com>
2024-02-02 18:42:59 +0100

    reviews

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -16,8 +16,9 @@ const Home = () => {
       <PhotoSlider />
       <AboutUs />
       <CommonQuestions />
-      <ContactUs />
       <Reviews />
+      <ContactUs />
+
       <Footer />
     </div>
   );
--- src/Reviews/Reviews.js
+++ src/Reviews/Reviews.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./reviews.css";
 import star from "../images/star-rating.png";
 import avatar from "../images/avatar.png";
+import erlind from "../images/erlind.png";
+import arlind from "../images/arlind.png";
+import arvanit from "../images/arvanit.png";
+import ertural from "../images/ertural.jpg";
 
 const Reviews = () => {
   return (
@@ -12,68 +16,119 @@ const Reviews = () => {
           <img className="star" src={star} alt="stars" />
           <p>"Another level"</p>
           <div className="profile">
-            <a href="https://www.google.com" target="_blank">
-              <img className="avatar" src={avatar} alt="profile" />
+            <a href="https://maps.app.goo.gl/VTxqnbtfoe34M5zQ8" target="_blank">
+              <img className="avatar" src={erlind} alt="profile" />
             </a>
-            <p>Name Surname</p>
+            <p>Erlind Behrami</p>
           </div>
+          <a href="https://maps.app.goo.gl/VTxqnbtfoe34M5zQ8" target="_blank">
+            Read more
+          </a>
         </div>
 
         <div className="review">
           <img className="star" src={star} alt="stars" />
-          <p>"Another level"</p>
+          <p>"🔥…"</p>
           <div className="profile">
-            <a href="https://www.google.com" target="_blank">
-              <img className="avatar" src={avatar} alt="profile" />
+            <a href="https://maps.app.goo.gl/MbaCtnkMGxqQaMqZ9" target="_blank">
+              <img className="avatar" src={arlind} alt="profile" />
             </a>
-            <p>Name Surname</p>
+            <p>Arlind Dubova</p>
           </div>
+          <a href="https://maps.app.goo.gl/MbaCtnkMGxqQaMqZ9" target="_blank">
+            Read more
+          </a>
         </div>
 
         <div className="review">
           <img className="star" src={star} alt="stars" />
-          <p>"Another level"</p>
+          {/* <p>"🌟🌟🌟🌟🌟"</p> */}
           <div className="profile">
-            <a href="https://www.google.com" target="_blank">
-              <img className="avatar" src={avatar} alt="profile" />
+            <a href="https://maps.app.goo.gl/UANaWzBP6QQYvMra8" target="_blank">
+              <img className="avatar" src={arvanit} alt="profile" />
             </a>
-            <p>Name Surname</p>
+            <p>Arvanit Shahini</p>
           </div>
+          <a href="https://maps.app.goo.gl/UANaWzBP6QQYvMra8" target="_blank">
+            Read more
+          </a>
         </div>
 
         <div className="review">
           <img className="star" src={star} alt="stars" />
-          <p>"Another level"</p>
+          <p>
+            "A paradise for the gamers who wanna give their best on the best
+            pc's out there"
+          </p>
           <div className="profile">
             <a href="https://www.google.com" target="_blank">
-              <img className="avatar" src={avatar} alt="profile" />
+              <img className="avatar" src={ertural} alt="profile" />
             </a>
-            <p>Name Surname</p>
+            <p>Ertural Koliqi</p>
           </div>
-        </div>
-
-        <div className="review">
-          <img className="star" src={star} alt="stars" />
-          <p>"Another level"</p>
-          <div className="profile">
-            <a href="https://www.google.com" target="_blank">
-              <img className="avatar" src={avatar} alt="profile" />
-            </a>
-            <p>Name Surname</p>
-          </div>
-        </div>
-
-        <div className="review">
-          <img className="star" src={star} alt="stars" />
-          <p>"Another level"</p>
-          <div className="profile">
-            <a href="https://www.google.com" target="_blank">
-              <img className="avatar" src={avatar} alt="profile" />
-            </a>
-            <p>Name Surname</p>
-          </div>
+          <a href="https://www.google.com" target="_blank">
+            Read more
+          </a>
         </div>
       </div>
+
+      {/* <div className="reviews secondpartreview">
+        <div className="review">
+          <img className="star" src={star} alt="stars" />
+          <p>"Another level"</p>
+          <div className="profile">
+            <a href="https://www.google.com" target="_blank">
+              <img className="avatar" src={avatar} alt="profile" />
+            </a>
+            <p>Name Surname</p>
+          </div>
+          <a href="https://www.google.com" target="_blank">
+            Read more
+          </a>
+        </div>
+
+        <div className="review">
+          <img className="star" src={star} alt="stars" />
+          <p>"Another level"</p>
+          <div className="profile">
+            <a href="https://www.google.com" target="_blank">
+              <img className="avatar" src={avatar} alt="profile" />
+            </a>
+            <p>Name Surname</p>
+          </div>
+          <a href="https://www.google.com" target="_blank">
+            Read more
+          </a>
+        </div>
+
+        <div className="review">
+          <img className="star" src={star} alt="stars" />
+          <p>"Another level"</p>
+          <div className="profile">
+            <a href="https://www.google.com" target="_blank">
+              <img className="avatar" src={avatar} alt="profile" />
+            </a>
+            <p>Name Surname</p>
+          </div>
+          <a href="https://www.google.com" target="_blank">
+            Read more
+          </a>
+        </div>
+
+        <div className="review">
+          <img className="star" src={star} alt="stars" />
+          <p>"Another level"</p>
+          <div className="profile">
+            <a href="https://www.google.com" target="_blank">
+              <img className="avatar" src={avatar} alt="profile" />
+            </a>
+            <p>Name Surname</p>
+          </div>
+          <a href="https://www.google.com" target="_blank">
+            Read more
+          </a>
+        </div>
+      </div> */}
     </div>
   );
 };
